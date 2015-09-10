a73793cc6066bf7b86cf866e8c45819cf4f159e3
TaffyQuinzel <louis.burg@hotmail.com>
2015-09-10 14:31:53 +0200

    ballsimulation is added and working. Chapter 1 in seperate file

--- friendly_tut/friendly_tut/Program.fs
+++ friendly_tut/friendly_tut/Program.fs
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+open Chapter1
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    do Chapter1.BallSimulation.simulation()
     0 // return an integer exit code
