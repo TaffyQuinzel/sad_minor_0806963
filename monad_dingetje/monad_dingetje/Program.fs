4cb6cb89e2aaefec1ed92e696ea111682316ac6d
TaffyQ <taffy_quinzel@hotmail.com>
2015-10-05 15:11:58 +0200

    state monad fixed

--- monad_dingetje/monad_dingetje/Program.fs
+++ monad_dingetje/monad_dingetje/Program.fs
@@ -1,8 +1,8 @@
 ﻿open System
 
 type option<'a> =
- | Just of 'a
- | None
+  | Just of 'a
+  | None
 
 let ret a = match a with | Just a -> a | None -> 0
 
@@ -29,15 +29,15 @@ let doFunc f a:option<'a> =
   | None -> None
 
 let add a b =
-   b + a
-  
+  b + a
+
 let i = Just 1
 let j = doFunc (add 1) i
 
 let x = sum i j
 
-printf "%A" (ret x)
-Console.ReadKey()
+// printf "%A" (ret x)
+// Console.ReadKey()
 
 type list<'a> =
   | Full of 'a * list<'a>
@@ -50,28 +50,87 @@ type ListBuilder() =
     | Empty -> Empty
   member this.Return x = Full x
   member this.Zero = Empty
-  
-let lst = ListBuilder()
-  
-let addToList (a:int) (b:list<'a>) =
-  lst{
-    return a :: b
-    
-  }
+
+// let lst = ListBuilder()
+
+// let addToList (a:int) (b:list<'a>) =
+//   lst{
+//     return a :: b
+
+//   }
 
 
 type State<'a,'s> = 's -> ('a * 's)
 
 type StateBuilder() =
-  member this.bind (o:State<'a,'s>, f:'s -> State<'b,'s>):State<'b,'s> =
+  member this.Bind (o:State<'a,'s>, f:'s -> State<'b,'s>):State<'b,'s> =
     fun s ->
-    let (a,s') = o s
-    let (b,s'') = f a s'
-    (b,s'')
-  member this.Return x y = ((fun (a:'a)  (s:'s) -> (a * s)) x y)
-  member this.Zero = ()
+      let (a,s') = o s
+      let (b,s'') = f a s'
+      (b,s'')
+  member this.Return x  = fun (s:'s) -> x,s  
+  // member this.Zero = (),o
+
+let sb = StateBuilder()
+
+  //----------------------------------------------
+type Result<'a> = Done of 'a | Crash of string
+
+
+type MaybeBuilder() =
+  member this.Bind (a:Result<'a>, b: 'a -> Result<'b>) : Result<'b>=
+    match a with
+      | Done c -> b c
+      | Crash e -> Crash e
+    // member this.Return a:'a = Done a
+    member this.ReturnFrom a:'a = a
+    // member this.zero = Crash "haha grapjas"
+    member this.zero = ()
+
+let mb = MaybeBuilder()
+
+
+type MaybeState<'a,'s> = 's -> (Result<'a> * 's)
+
+type MaybeStateBuilder() =
+  member this.Bind (o:MaybeState<'a,'s>, f:'s -> MaybeState<'b,'s>) : MaybeState<'b,'s> =
+    fun (s:'s) ->
+      let x,y = o s
+      match x with
+        | Done a ->
+          let v,w = f a y
+          match v with
+            | Done b -> v,w
+            | Crash b -> Crash b,w
+        | Crash a -> Crash a,y
+  member this.ReturnFrom x  = (fun s -> Done,s) x 
+  member this.Zero = Crash
+
+
+let ms = MaybeStateBuilder()
+
+let f1 a = (fun o -> o) a
+let f2 a:State<'a,'s> = (fun o -> o) a
+
+let stateTest (i) =
+  sb{
+    return i
+  }
+let stateTest2 (i:State<'a,'s>) =
+  sb{
+    let! a = i 
+    return (a + 4)
+  }
+
+// let msTest (a:int)(w:int)=
+//   ms{
+//     let! a = (f1,1)
+//     let! w = f2 9
+//     return! w
+//   }
+
   
-  
+
 //Type OptionBuilder (x:option<'a>) (y:option<'b>) = {
 //    this.bind(x,y)
 //    let! x_v = fun Just x -> x
@@ -79,7 +138,18 @@ type StateBuilder() =
 //    ret (x_v + y_v)
 //  }
 
+
+// type MaybeStateList<'a,'s> = 's -> (List<'a>,'s)
+
+
+// type ListMaybeState<list<'a,'s>> = list<> -> 
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+  // printfn "%A" argv
+  let hal = stateTest 4
+  printfn "%A" (hal)
+  printfn "%A" (stateTest2 hal)
+  printfn "%A" (stateTest 9)
+  0 // return an integer exit code
